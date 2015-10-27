0a386ba718b3ec244fd0a4e1c06c3f99ed3a3820
Moises <47767573t@iespoblenou.org>
2015-10-27 19:49:45 +0100

    anyadido listView

--- app/src/main/java/com/primapp/movieapp/MainActFragment.java
+++ app/src/main/java/com/primapp/movieapp/MainActFragment.java
@@ -5,11 +5,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActFragment extends Fragment {
+
+
+    private ArrayList<String> listMovie = new ArrayList<String>();
+    private ArrayAdapter adapterMovie;
+
+    private ListView lvMovie;
+
+    private View vRoot;
+
 
     public MainActFragment() {
     }
@@ -17,6 +30,13 @@ public class MainActFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        vRoot = inflater.inflate(R.layout.fragment_main, container, false);
+        return vRoot;
     }
+
+
+
+
+
 }
