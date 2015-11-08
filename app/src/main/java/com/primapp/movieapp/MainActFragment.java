86052d70a58fc208916e8b31c646e6ea7e432459
MVDam <47767573t@iespoblenou.org>
2015-11-08 19:22:12 +0100

    añadido base url de la api

--- app/src/main/java/com/primapp/movieapp/MainActFragment.java
+++ app/src/main/java/com/primapp/movieapp/MainActFragment.java
@@ -41,6 +41,8 @@ public class MainActFragment extends Fragment {
     private View vRoot;
     Boolean menu = true;
 
+    private String UrlBase;
+
     public MainActFragment() {
     }
 
@@ -101,12 +103,13 @@ public class MainActFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        //refresh();
+        refresh();
     }
 
     private void refresh(){
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://api.themoviedb.org/3/movie/550?")
+                .baseUrl("http://api.themoviedb.org/3/movie/")
+                .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
         MovieService service = retrofit.create(MovieService.class);
